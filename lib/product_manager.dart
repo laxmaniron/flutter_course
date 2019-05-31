0286bffaf28bc9bea2432b1ea3198f1b40e1d307
laxmaniron <laxmankumarapu@gmail.com>
2019-05-31 22:47:23 +0530

    passing arguments refrencing function of one widget from the other

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
 
@@ -31,19 +32,17 @@ class _ProductManagerState extends State<ProductManager> {
     super.didUpdateWidget(oldWidget);
   }
 
+  void _addProduct(String product){
+     setState(() {
+            _products.add(product);
+          });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[Container(
       margin: EdgeInsets.all(10.0),
-      child: RaisedButton(
-        color: Theme.of(context).primaryColor,
-        onPressed: () {
-          setState(() {
-            _products.add('Avatar');
-          });
-        },
-        child: Text("Add Product"),
-      ),
+      child: ProductControl(_addProduct),
     ),
     Products(_products)
     ],);
