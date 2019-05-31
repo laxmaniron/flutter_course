5328a4d9a6e1a0a79110e744628fa43d13152d15
laxmaniron <laxmankumarapu@gmail.com>
2019-05-31 06:18:02 +0530

    type of variable declaration in important in Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('EasyList'),
+          title: Text('Easy List'),
         ),
       ),
     );
