a6b178976a8626d5639f15fbb89bb5453ceec6c4
laxmaniron <laxmankumarapu@gmail.com>
2019-05-31 16:39:05 +0530

    stateful widget boilerplate syntax

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,17 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
+     
+     @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
