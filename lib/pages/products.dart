60f5e975edeaa833eb206ff03ffd5c55b7dcfd09
laxmaniron <laxmankumarapu@gmail.com>
2019-06-02 00:11:52 +0530

    adding the side drawer and the hamburger icon

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -3,10 +3,23 @@ import 'package:flutter/material.dart';
 import '../product_manager.dart';
 
 class ProductsPage extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('Easy List'),
       ),
