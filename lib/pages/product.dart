8656ce7259c31e6ee48316c0dc83ec947878e00b
laxmaniron <laxmankumarapu@gmail.com>
2019-06-01 23:47:15 +0530

    fixing auto popback button session 72

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,36 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
-
   final String title;
   final String imageUrl;
 
-  ProductPage(this.title,this.imageUrl);
+  ProductPage(this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text(title),
-          ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: RaisedButton(
-
-              color: Theme.of(context).accentColor,
-              child: Text('DELETE'),
-              onPressed: () => Navigator.pop(context, true),
+    return WillPopScope(
+      onWillPop:() {
+        //print("Back Button Pressed!");
+        Navigator.pop(context,false);   //pop back
+        return Future.value(false);   // Ignore original pop take custom popping mechanism
+      },
+      child:Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(title),
             ),
-          )
-        ],
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('DELETE'),
+                onPressed: () => Navigator.pop(context, true),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
