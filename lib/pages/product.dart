ce3d5567fcd311ada9ad0f3ac110369caf37f5b0
laxmaniron <laxmankumarapu@gmail.com>
2019-06-01 22:06:59 +0530

    implementing a basic navigation session 68

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
-class ProductPage extends StatelessWidget{
+class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: Text("Product Detail"),
       ),
-      body: Center(
-        child: Text('On the Product Page'),
+      body: Column(
+        children: <Widget>[
+          Text("Details!"),
+          RaisedButton(
+            child: Text('BACK'),
+            onPressed: () => Navigator.pop(context),
+          )
+        ],
       ),
     );
   }
--- lib/products.dart
+++ lib/products.dart
@@ -1,54 +1,57 @@
 import 'package:flutter/material.dart';
 
+import './pages/product.dart';
+
 class Products extends StatelessWidget {
   final List<String> products;
 
   Products([this.products = const []]);
 
-  Widget _buildProductItem(BuildContext context, int index){
+  Widget _buildProductItem(BuildContext context, int index) {
     return Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/mi6.png'),
-                      Text(products[index]),
-                      ButtonBar(
-                        alignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          FlatButton(
-                            child: Text('Details'),
-                            onPressed: () => {},
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/mi6.png'),
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => ProductPage(),
+                      ),
+                    ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 
-  Widget _buildProductList(){
-
+  Widget _buildProductList() {
     Widget productCards;
 
-    if(products.length>0){
-      productCards= ListView.builder(
-         itemBuilder: _buildProductItem,
-         itemCount: products.length,
-    );
-    }
-    else {
+    if (products.length > 0) {
+      productCards = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
       // productCards = Container();
       productCards = Center(
-      child: Text("No Products found, please add some"),
+        child: Text("No Products found, please add some"),
       );
-    }  
+    }
 
     return productCards;
-
   }
 
   @override
   Widget build(BuildContext context) {
-
     return _buildProductList();
   }
 }
