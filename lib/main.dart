56aa54182783aaef1e0701c0b5499ee48c8f58bc
laxmaniron <laxmankumarapu@gmail.com>
2019-06-02 01:38:27 +0530

    adding named routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 
 import './pages/auth.dart';
+import './pages/products_admin.dart';
+import './pages/products.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
@@ -23,7 +25,12 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.deepPurple
       ),
-      home: AuthPage(),
+      // home: AuthPage(),
+
+      routes:{
+        '/':(BuildContext context) => ProductsPage(),
+        '/admin': (BuildContext context) => ProductsAdminPage(),
+      } ,
     );
   }
 }
