876046adb12150b0a22ef443358e07309790fcbc
laxmaniron <laxmankumarapu@gmail.com>
2019-06-02 00:01:30 +0530

    adding basic replacement routes and Basic authentication page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 
-import './pages/home.dart';
+import './pages/auth.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.deepPurple
       ),
-      home: HomePage(),
+      home: AuthPage(),
     );
   }
 }
